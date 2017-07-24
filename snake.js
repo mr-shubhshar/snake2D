90b8ad5d63609df1b0f5be247a8cf8fbe5c0cbdc
Shubham Sharma <mr.shubhshar@gmail.com>
2017-07-25 00:48:53 +0530

    added direction and scale

--- snake.js
+++ snake.js
@@ -5,12 +5,20 @@ function Snake() {
     this.yspeed = 0;
 
     this.update = function() {
-        this.x += this.xspeed;
-        this.y += this.yspeed;
+        this.x += this.xspeed * scl;
+        this.y += this.yspeed * scl;
+
+        this.x = constrain(this.x, 0, width-scl);
+        this.y = constrain(this.y, 0, height-scl);
     }
 
     this.show = function() {
         fill(255);
         rect(this.x, this.y, 10, 10);
+    }
+
+    this.dir = function(x, y) {
+        this.xspeed = x;
+        this.yspeed = y;
     }
 }
